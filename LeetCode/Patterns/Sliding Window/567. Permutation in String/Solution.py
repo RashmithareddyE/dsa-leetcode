ba2ce0763748aa242c26a2d8615fddf4912ec6f8
RashmithareddyE <erashmitha2007@gmail.com>
2026-08-22 17:07:35 +0530

    Add Python Solution to Patterns/Sliding Window

--- LeetCode/Patterns/Sliding Window/567. Permutation in String/Solution.py
+++ LeetCode/Patterns/Sliding Window/567. Permutation in String/Solution.py
@@ -1,30 +1,30 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
 
-        result = []
+        if len(s1) > len(s2):
+            return False
 
-        def backtrack(path, used):
+        count1 = [0] * 26
+        count2 = [0] * 26
 
-            if len(path) == len(s1):
-                result.append("".join(path))
-                return
+        # Frequency of s1
+        for c in s1:
+            count1[ord(c) - ord('a')] += 1
 
-            for i in range(len(s1)):
+        left = 0
 
-                if used[i]:
-                    continue
+        for right in range(len(s2)):
 
-                path.append(s1[i])
-                used[i] = True
+            # Add s2[right]
+            count2[ord(s2[right]) - ord('a')] += 1
 
-                backtrack(path, used)
+            # Keep window size equal to s1
+            if right - left + 1 > len(s1):
+                count2[ord(s2[left]) - ord('a')] -= 1
+                left += 1
 
-                path.pop()
-                used[i] = False
-
-        backtrack([], [False] * len(s1))
-        for p in result:
-            if p in s2:
+            # Check frequency match
+            if count1 == count2:
                 return True
 
         return False
