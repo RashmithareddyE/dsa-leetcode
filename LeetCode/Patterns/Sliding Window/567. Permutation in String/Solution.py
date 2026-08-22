8fb0574b0c3ea576576ea1b55cf6a7c9fe760023
RashmithareddyE <erashmitha2007@gmail.com>
2026-08-22 17:07:58 +0530

    Add Python Solution to Patterns/Sliding Window

--- LeetCode/Patterns/Sliding Window/567. Permutation in String/Solution.py
+++ LeetCode/Patterns/Sliding Window/567. Permutation in String/Solution.py
@@ -7,7 +7,6 @@ class Solution:
         count1 = [0] * 26
         count2 = [0] * 26
 
-        # Frequency of s1
         for c in s1:
             count1[ord(c) - ord('a')] += 1
 
@@ -15,15 +14,12 @@ class Solution:
 
         for right in range(len(s2)):
 
-            # Add s2[right]
             count2[ord(s2[right]) - ord('a')] += 1
 
-            # Keep window size equal to s1
             if right - left + 1 > len(s1):
                 count2[ord(s2[left]) - ord('a')] -= 1
                 left += 1
 
-            # Check frequency match
             if count1 == count2:
                 return True
 
